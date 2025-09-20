f066c3c6d16d606660ac96498186565bcae59a18
Momen <mominitmbstu@gmail.com>
2025-09-20 15:15:24 +0600

    email pass auth last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import MainLayout from './MainLayout/MainLayout';
-import Home from './Component/Home/Home';
-import Login from './Component/Login/Login';
-import Registration from './Component/Registration/Registration';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./MainLayout/MainLayout";
+import Home from "./Component/Home/Home";
+import Login from "./Component/Login/Login";
+import Registration from "./Component/Registration/Registration";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout/>,
+    element: <MainLayout />,
     children: [
       {
         path: "/",
-        element: <Home></Home>
+        element: <Home></Home>,
       },
       {
         path: "/login",
-        element: <Login></Login>
+        element: <Login></Login>,
       },
       {
         path: "/reg",
-        element: <Registration></Registration>
-      }
-    ]
+        element: <Registration></Registration>,
+      },
+    ],
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
